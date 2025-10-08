ab6cfde3548ea6e2120155a1be4aa505a6f72712
froelen <187111392+froelen@users.noreply.github.com>
2025-10-08 13:06:00 +0200

    simple test avec pygame pour faire une fenetre t un déplacement latéral

--- bases.py
+++ bases.py
@@ -5,3 +5,28 @@ pygame.init()
 win = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption('test game')
+
+
+
+run = True
+x, y, width, height, vel = 50, 50, 40, 60, 5
+
+while run:
+    pygame.time.delay(100)
+    keys = pygame.key.get_pressed()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+        
+    elif keys[pygame.K_RIGHT]:
+        x += vel
+
+    win.fill((0, 0, 0))
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    pygame.display.update()
+
+pygame.quit()
